5cc004ba8cc34bdc44745fe89c21e99f92a06253
Mike Butsko <butsko.7@osu.edu>
2012-05-29 19:59:52 -0400

    Letters on people page are now jump to.

--- letter_jump.js
+++ letter_jump.js
@@ -1,7 +1,7 @@
 Drupal.behaviors.myModuleBehavior = function (context) {
 
   $("a").click(function(){
-    var letter = this.html();
+    var letter = $(this).html();
     var firstItem = $("a." + letter).filter(':first');
     var top = firstItem.offset().top;
     $("body,html").animate({scrollTop: top}, 800);
